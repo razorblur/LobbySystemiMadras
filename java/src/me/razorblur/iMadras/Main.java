85b8f2d893623a9b7db76e77a4c739b641e49156
Nicolai Helldorff <razorblurhd@gmail.com>
2015-06-05 15:16:07 +0200

    Initial Commit 02

--- java/src/me/razorblur/iMadras/Main.java
+++ java/src/me/razorblur/iMadras/Main.java
@@ -6,4 +6,15 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by Nicolai on 05.06.2015.
  */
 public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
